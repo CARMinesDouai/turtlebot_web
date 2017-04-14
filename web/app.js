9f6ecfa8eddffeea74c7b07c9ecec9947479d5c9
Christopher Lievin <lievin.christopher@gmail.com>
2017-04-14 11:53:16 +0200

    add server request file

--- web/app.js
+++ web/app.js
@@ -1,12 +1,46 @@
-var http = require('http');
-var fs = require('fs');
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(content);
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8080;
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+
+  var contentTypesByExtension = {
+    '.html': "text/html",
+    '.css':  "text/css",
+    '.js':   "text/javascript"
+  };
+
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      var headers = {};
+      var contentType = contentTypesByExtension[path.extname(filename)];
+      if (contentType) headers["Content-Type"] = contentType;
+      response.writeHead(200, headers);
+      response.write(file, "binary");
+      response.end();
     });
-});
+  });
+}).listen(parseInt(port, 10));
 
-
-server.listen(8080);
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
